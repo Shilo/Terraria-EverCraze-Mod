62f375b495bfa2c3355cbc101c74b4d1ff4e853c
Shilo White <shilo86@gmail.com>
2017-07-03 00:39:51 -0700

    Added Fuuturo sword.

--- Items/Fuuturo.cs
+++ Items/Fuuturo.cs
@@ -1,3 +1,5 @@
+using Microsoft.Xna.Framework;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -8,31 +10,24 @@ namespace EverCraze.Items
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Fuuturo");
-			Tooltip.SetDefault("This is a modded sword.");
-		}
-		public override void SetDefaults()
-		{
-			item.damage = 50;
-			item.melee = true;
-			item.width = 40;
-			item.height = 40;
-			item.useTime = 20;
-			item.useAnimation = 20;
-			item.useStyle = 1;
-			item.knockBack = 6;
-			item.value = 10000;
-			item.rare = 2;
-			item.UseSound = SoundID.Item1;
-			item.autoReuse = true;
+			Tooltip.SetDefault("The elegant sword once wielded by almighty Savannah.");
 		}
 
-		public override void AddRecipes()
+		public override void SetDefaults()
 		{
-			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(ItemID.DirtBlock, 10);
-			recipe.AddTile(TileID.WorkBenches);
-			recipe.SetResult(this);
-			recipe.AddRecipe();
+            item.CloneDefaults(ItemID.BreakerBlade);
+            item.autoReuse = true;
 		}
-	}
+
+        public override void OnHitNPC(Player player, NPC target, int damage, float knockBack, bool crit)
+        {
+            target.AddBuff(BuffID.OnFire, 5 * 60);
+        }
+
+        public override void MeleeEffects(Player player, Rectangle hitbox)
+        {
+            int dust = Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, DustID.Fire, player.velocity.X * 0.2f + (float)(player.direction * 3), player.velocity.Y * 0.2f, 100, default(Color), 3.0f);
+            Main.dust[dust].noGravity = true;
+        }
+    }
 }
